b8dc3bbd3e65d8643f2d7d94840123815a6eb3e3
AnnaHort <annahort3d@gmail.com>
2023-12-13 01:01:56 +0200

    deleting unnecessary files

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,9 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Header } from 'components/Header/Header';
 
 const SharedLayout = () => {
   return (
-    <>
-      <Header />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
-    </>
+    <h1>
+     Hello!!
+    </h1>
   );
 };
 
