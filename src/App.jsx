cd2216ef61d8b7b76e09ae47d23df06e3166f67f
AnnaHort <annahort3d@gmail.com>
2023-12-13 20:18:20 +0200

    deleting test pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import SharedLayout from 'components/SharedLayout/SharedLayout';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SharedLayout />}></Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
--- src/components/SharedLayout/SharedLayout.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const TitleStyled = styled.h1`
-color: var(--color-primary-grey);
-font-size: 22px;
-font-weight: 700;
-line-height: 1.5;
-`
