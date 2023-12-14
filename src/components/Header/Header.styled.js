89a20cef702717148472990580bf58cf7768dfe8
AnnaHort <annahort3d@gmail.com>
2023-12-14 11:32:00 +0200

    debuging main style added some header style

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
 export const HeaderContainerStyled = styled.div`
   display: flex;
+  justify-content: space-between;
+  background-color: var(--color-primary-black-2);
+  padding: 19px 10px;
+  align-items: center;
 `;
---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const NavContainerStyled = styled.nav`
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+`
