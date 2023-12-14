a08530b96b92d68e4e26766628a944c1ee999325
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-14 23:27:07 +0200

    Create SignUpRegister.jsx

---
+++ src/components/SignUpForm/SignUpRegister.jsx
@@ -0,0 +1,58 @@
+import {
+  SignUpRegisterContainer,
+  Image,
+  RegisterTitle,
+  RegisterText,
+  RegisterForm,
+  InputContainer,
+  InputText,
+  InputButton,
+  QuestionBlock,
+  Question,
+  Link,
+} from './SignUpRegister.styled';
+import RegisterImage from "../../img/register-img-mobile.png"
+
+const SignUpRegister = () => {
+  return (
+    <SignUpRegisterContainer>
+      <Image src={RegisterImage} alt="image" />
+      <RegisterTitle>Sign up</RegisterTitle>
+      <RegisterText>You need to register to use the service</RegisterText>
+      <RegisterForm autoComplete="off">
+        <label htmlFor="name">
+          <InputContainer>
+            <InputText type="text" id="name" name="name" placeholder="Name" />
+          </InputContainer>
+        </label>
+        <label htmlFor="email">
+          <InputContainer>
+            <InputText
+              type="email"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+            />
+          </InputContainer>
+        </label>
+        <label htmlFor="password">
+          <InputContainer>
+            <InputText
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </InputContainer>
+        </label>
+        <InputButton type="submit">Sign Up</InputButton>
+      </RegisterForm>
+      <QuestionBlock>
+        <Question>Do you already have an account?</Question>
+        <Link to="/signin">Sign in</Link>
+      </QuestionBlock>
+    </SignUpRegisterContainer>
+  );
+};
+
+export default SignUpRegister;
