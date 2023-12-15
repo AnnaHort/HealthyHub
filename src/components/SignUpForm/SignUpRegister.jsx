f0fdbfd5f2e52abad222268a6c56506d861130a1
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-15 02:32:53 +0200

    Update SignUpRegister.jsx

--- src/components/SignUpForm/SignUpRegister.jsx
+++ src/components/SignUpForm/SignUpRegister.jsx
@@ -5,52 +5,54 @@ import {
   RegisterText,
   RegisterForm,
   InputContainer,
-  InputText,
+  Input,
   Button,
   QuestionBlock,
   Question,
   Link,
 } from './SignUpRegister.styled';
-import RegisterImage from "../../img/register-img-mobile.png"
+import RegisterImage from '../../img/register-img-mobile.png';
 
 const SignUpRegister = () => {
   return (
     <SignUpRegisterContainer>
       <Image src={RegisterImage} alt="image" />
-      <RegisterTitle>Sign up</RegisterTitle>
-      <RegisterText>You need to register to use the service</RegisterText>
-      <RegisterForm autoComplete="off">
-        <label htmlFor="name">
-          <InputContainer>
-            <InputText type="text" id="name" name="name" placeholder="Name" />
-          </InputContainer>
-        </label>
-        <label htmlFor="email">
-          <InputContainer>
-            <InputText
-              type="email"
-              id="email"
-              name="email"
-              placeholder="E-mail"
-            />
-          </InputContainer>
-        </label>
-        <label htmlFor="password">
-          <InputContainer>
-            <InputText
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-            />
-          </InputContainer>
-        </label>
-        <Button type="submit">Next</Button>
-      </RegisterForm>
-      <QuestionBlock>
-        <Question>Do you already have an account?</Question>
-        <Link to="/signin">Sign in</Link>
-      </QuestionBlock>
+      <div>
+        <RegisterTitle>Sign up</RegisterTitle>
+        <RegisterText>You need to register to use the service</RegisterText>
+        <RegisterForm autoComplete="off">
+          <label htmlFor="name">
+            <InputContainer>
+              <Input type="text" id="name" name="name" placeholder="Name" />
+            </InputContainer>
+          </label>
+          <label htmlFor="email">
+            <InputContainer>
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="E-mail"
+              />
+            </InputContainer>
+          </label>
+          <label htmlFor="password">
+            <InputContainer>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </InputContainer>
+          </label>
+          <Button type="submit">Next</Button>
+          <QuestionBlock>
+            <Question>Do you already have an account?</Question>
+            <Link to="/signin">Sign in</Link>
+          </QuestionBlock>
+        </RegisterForm>
+      </div>
     </SignUpRegisterContainer>
   );
 };
