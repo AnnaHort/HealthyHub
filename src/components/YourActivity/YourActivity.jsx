5cf28ed6f9e596eb29a7b82e14e9e90f7a8ae80b
AnnaHort <annahort3d@gmail.com>
2023-12-16 20:43:56 +0200

    Update YourActivity.jsx

--- src/components/YourActivity/YourActivity.jsx
+++ src/components/YourActivity/YourActivity.jsx
@@ -26,14 +26,15 @@ const YourActivity = () => {
         <YourActivityFormStyled action="#">
           <YourActivityList>
             <RadioButtonContainerStyled>
-              <RadioInputStyle type="radio" id="radio1" name="radiogroup" />
+
+              <RadioInputStyle type="radio" id="radio1" name="radiogroup" tabindex="0"/>
               <RadioLabelStyle htmlFor="radio1">
                 1.2 - if you do not have physical activity and sedentary work
               </RadioLabelStyle>
             </RadioButtonContainerStyled>
 
             <RadioButtonContainerStyled>
-              <RadioInputStyle type="radio" id="radio2" name="radiogroup" />
+              <RadioInputStyle type="radio" id="radio2" name="radiogroup" tabindex="0"/>
               <RadioLabelStyle htmlFor="radio2">
                 1.375 - if you do short runs or light gymnastics 1-3 times a
                 week
@@ -41,21 +42,21 @@ const YourActivity = () => {
             </RadioButtonContainerStyled>
 
             <RadioButtonContainerStyled>
-              <RadioInputStyle type="radio" id="radio3" name="radiogroup" />
+              <RadioInputStyle type="radio" id="radio3" name="radiogroup" tabindex="0"/>
               <RadioLabelStyle htmlFor="radio3">
                 1.55 - if you play sports with average loads 3-5 times a week
               </RadioLabelStyle>
             </RadioButtonContainerStyled>
 
             <RadioButtonContainerStyled>
-              <RadioInputStyle type="radio" id="radio4" name="radiogroup" />
+              <RadioInputStyle type="radio" id="radio4" name="radiogroup" tabindex="0"/>
               <RadioLabelStyle htmlFor="radio4">
                 1.725 - if you train fully 6-7 times a week
               </RadioLabelStyle>
             </RadioButtonContainerStyled>
 
             <RadioButtonContainerStyled>
-              <RadioInputStyle type="radio" id="radio5" name="radiogroup" />
+              <RadioInputStyle type="radio" id="radio5" name="radiogroup" tabindex="0"/>
               <RadioLabelStyle htmlFor="radio5">
                 1.9 - if your work is related to physical labor, you train 2
                 times a day and include strength exercises in your training
