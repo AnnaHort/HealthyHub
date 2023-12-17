dcf0c260d354071a76fbf91b82677d23c2410867
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-17 12:41:01 +0200

    add api backend

--- src/components/SignUpForm/SignUpRegister.jsx
+++ src/components/SignUpForm/SignUpRegister.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+ import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 import {
   SignUpRegisterContainer,
   Image,
   RegisterTitle,
   RegisterText,
   RegisterForm,
-  InputContainer,
+  //InputContainer,
   Input,
   Button,
   QuestionBlock,
@@ -15,7 +21,47 @@ import RegisterImageMobile from '../../img/register-img-mobile.png';
 import RegisterImageTablet from '../../img/register-img-tablet.png';
 import RegisterImageDesktop from '../../img/register-img-desktop.png';
 
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
+
+
 const SignUpRegister = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://healthhub-backend.onrender.com/api/auth/signup',
+        formData
+      );
+
+      // Обработка успешного ответа от сервера
+      console.log('Успешная регистрация', response.data);
+    } catch (error) {
+      // Обработка ошибки
+      console.error('Ошибка регистрации', error);
+    }
+  };
   return (
     <SignUpRegisterContainer>
       <Image
@@ -27,31 +73,37 @@ const SignUpRegister = () => {
       <div>
         <RegisterTitle>Sign up</RegisterTitle>
         <RegisterText>You need to register to use the service</RegisterText>
-        <RegisterForm autoComplete="off">
+          
+        <RegisterForm autoComplete="off" onSubmit={handleSubmit}>
           <label htmlFor="name">
-            <InputContainer>
-              <Input type="text" id="name" name="name" placeholder="Name" />
-            </InputContainer>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
           </label>
           <label htmlFor="email">
-            <InputContainer>
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="E-mail"
-              />
-            </InputContainer>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
           </label>
           <label htmlFor="password">
-            <InputContainer>
-              <Input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-              />
-            </InputContainer>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
           </label>
           <Button type="submit">Next</Button>
           <QuestionBlock>
