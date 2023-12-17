0669437d891145ba8ccd8fb7cd0bba3dc353428e
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-17 23:29:55 +0200

    fix img

--- src/pages/SignInPages/SignInPages.jsx
+++ src/pages/SignInPages/SignInPages.jsx
@@ -6,8 +6,8 @@ import {
   RegisterText,
   RegisterTitle,
 } from '../../components/SignUpForm/SignUpRegister.styled';
-import RegisterImageMobile from '../../img/register-img-mobile.png';
-import RegisterImageTablet from '../../img/register-img-tablet.png';
+//import RegisterImageMobile from '../../img/register-img-mobile.png';
+//import RegisterImageTablet from '../../img/register-img-tablet.png';
 import RegisterImageDesktop from '../../img/register-img-desktop.png';
 import {
   ButtonSignIn,
@@ -23,33 +23,22 @@ import {
 const SignInPages = () => {
   return (
     <SignInPagesContainer>
-      <Image
-        src={RegisterImageMobile}
-        srcSet={`${RegisterImageTablet} 834w, ${RegisterImageDesktop} 1440w`}
-        sizes="(max-width: 833px) 100vw, (min-width: 834px) 50vw"
-        alt="Responsive Image"
-      />
+      <Image src={RegisterImageDesktop} alt="Image" />
       <RegisterTitleContainer>
         <RegisterTitle>Sign in</RegisterTitle>
         <RegisterText>You need to login to use the service</RegisterText>
 
         <RegisterForm autoComplete="off">
           <label htmlFor="email">
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="E-mail"
-              />
+            <Input type="email" id="email" name="email" placeholder="E-mail" />
           </label>
           <label htmlFor="password">
-
-              <Input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-              />
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
           </label>
           <ForgotElementContainer>
             <SignInBtnContainer>
