00fc8143b211d824daa1c66ad331136ddb2758b8
Yakimenko Viktor <60235171+chell-iz-naroda@users.noreply.github.com>
2023-12-18 23:44:54 +0200

    add Main pages

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,22 +1,51 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { logOut } from "../../redux/auth/authOperations";
 import { Link, useNavigate } from "react-router-dom";
 
+import { ReactComponent as ArrowRigth } from '../../img/MainPages/arrow-right.svg';
+
+import {
+  Container,
+  MainContainer,
+  MainTitle,
+  MainLinkToGoal,
+  MainText,
+  MainWrapperTitle,
+  MainElementsWrapper,
+  DARFWrap,
+} from './MainPage.styled';
+
 const MainPage = () => {
   const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   const handleLogout = async () => {
-    await dispatch(logOut());
-    navigate("/signin");
+    try {
+      await dispatch(logOut());
+      navigate("/signin");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
   };
 
   return (
-    <Link to="/signin" onClick={handleLogout}>
+    <Container>
+      <MainContainer>
+        <MainWrapperTitle>
+          <MainTitle>Today</MainTitle>
+          <MainLinkToGoal to="dashboard">
+            <MainText>On the way to the goal</MainText>
+            <ArrowRigth />
+          </MainLinkToGoal>
+        </MainWrapperTitle>
+      </MainContainer>
+      <Link to="/signin" onClick={handleLogout}>
       Logout
     </Link>
+    </Container>
   );
 };
 
 export default MainPage;
-
