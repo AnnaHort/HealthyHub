83abf2f3312e87c5b53d46473f61d6854940cc04
AnnaHort <annahort3d@gmail.com>
2023-12-20 13:29:24 +0200

    added private and restricted routes

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,18 +2,22 @@ import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import { HeaderContainerStyled } from '../Header/Header.styled';
 import { Suspense } from 'react';
+import HeaderAuthorizedUser from '../HeaderAuthorizedUser/HeaderAuthorizedUser';
+import { useSelector } from 'react-redux';
+import selectIsLoggedIn from '../../redux/auth/authSelectors';
 
 const SharedLayout = () => {
- 
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div>
       <header>
         <HeaderContainerStyled>
-          <Header />
+          {isLoggedIn ? <HeaderAuthorizedUser /> : <Header />}
         </HeaderContainerStyled>
       </header>
 
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
     </div>
