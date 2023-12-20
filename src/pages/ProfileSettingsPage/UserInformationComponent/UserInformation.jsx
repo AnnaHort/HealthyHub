abc003e5cb03a67c499a2f008088e4e92a331310
AnnaHort <annahort3d@gmail.com>
2023-12-20 22:57:49 +0200

    starting styled ProfileSettingPages

--- src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.jsx
+++ src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.jsx
@@ -1,76 +1,159 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import {
+  UserInformationContainer,
+  UserInformationDownloadLabel,
+  UserInformationForm,
+  UserInformationIconDirect,
+  UserInformationInput,
+  UserInformationLabel,
+  UserInformationPhotoContainer,
+} from './UserInformation.styled';
+import { ImgContainer } from '../../../components/UserInfoNav/UserInfoNav.styled';
 
 const UserInformation = () => {
   return (
-    <form>
-      <label htmlFor="name">Your name</label>
-      <input type="text" id="name" placeholder="тут має бути імя користувача" />
+    <UserInformationForm>
+      <UserInformationContainer>
+        <UserInformationLabel htmlFor="name">Your name</UserInformationLabel>
+        <UserInformationInput
+          type="text"
+          id="name"
+          placeholder="тут має бути імя користувача"
+        />
+      </UserInformationContainer>
 
-      <label htmlFor="photo">Your photo</label>
-      <input type="file" id="photo" name="fileUpload" />
+      <UserInformationContainer>
+        <UserInformationLabel htmlFor="photo">Your photo</UserInformationLabel>
+        <UserInformationPhotoContainer>
+          <ImgContainer
+            style={{ marginRight: '12px', width: '36px', height: '36px' }}
+          >
+            <img
+              style={{ width: '36px', height: '36px' }}
+              src="/src/components/UserInfoNav/Avatar.svg"
+              alt="Avatar"
+            />
+          </ImgContainer>
+          <UserInformationIconDirect>
+            <use
+              href="/src/Sprites/icons/symbol-defs.svg#icon-direct-inbox"
+              stroke="var(--color-primary-green-lite)"
+            ></use>
+          </UserInformationIconDirect>
+          <input type="file" id="photo" name="fileUpload" style={{position:"absolute", opacity:"0"}}/>
+          <UserInformationDownloadLabel htmlFor="photo">Download new photo</UserInformationDownloadLabel>
+        </UserInformationPhotoContainer>
+      </UserInformationContainer>
 
-      <label htmlFor="age">Your age</label>
-      <input type="number" id="age" name="quantity" min="1" max="100" />
+      <UserInformationContainer>
+        <UserInformationLabel htmlFor="age">Your age</UserInformationLabel>
+        <UserInformationInput
+          type="number"
+          id="age"
+          name="quantity"
+          min="1"
+          max="100"
+        />
+      </UserInformationContainer>
 
-      <h3>Gender</h3>
-      <label htmlFor="">Male</label>
-      <input type="radio" id="male" name="gender" value="male" />
+      <UserInformationContainer>
+        <h3>Gender</h3>
+        <div>
+          <label htmlFor="male">Male</label>
+          <input type="radio" id="male" name="gender" value="male" />
+          <label htmlFor="female">Female</label>
+          <input type="radio" id="female" name="gender" value="female" />
+        </div>
+      </UserInformationContainer>
 
-      <label htmlFor="">Female</label>
-      <input type="radio" id="female" name="gender" value="female" />
+      <UserInformationContainer>
+        <UserInformationLabel htmlFor="height">Height</UserInformationLabel>
+        <UserInformationInput
+          type="number"
+          id="height"
+          name="quantity"
+          min="1"
+          max="100"
+        />
+      </UserInformationContainer>
 
-      <label htmlFor="height">Height</label>
-      <input type="number" id="height" name="quantity" min="1" max="100" />
+      <UserInformationContainer>
+        <UserInformationLabel htmlFor="weight">Weight</UserInformationLabel>
+        <UserInformationInput
+          type="number"
+          id="weight"
+          name="quantity"
+          min="1"
+          max="100"
+        />
+      </UserInformationContainer>
 
-      <label htmlFor="weight">Weight</label>
-      <input type="number" id="weight" name="quantity" min="1" max="100" />
+      <UserInformationContainer>
+        <h3>Your Activity</h3>
+        <div>
+          <div>
+            <label htmlFor="low 1.2">
+              1.2 - if you do not have physical activity and sedentary work
+            </label>
+            <input type="radio" id="low 1.2" value="low 1.2" name="activity" />
+          </div>
 
-      <h3>Your Activity</h3>
+          <div>
+            <label htmlFor="light 1.375">
+              1.375 - if you do short runs or light gymnastics 1-3 times a week
+            </label>
+            <input
+              type="radio"
+              id="light 1.375"
+              value="light 1.375"
+              name="activity"
+            />
+          </div>
 
-      <label htmlFor="low 1.2">
+          <div>
+            <label htmlFor="average 1.55">
+              1.55 - if you play sports with average loads 3-5 times a week
+            </label>
+            <input
+              type="radio"
+              id="average 1.55"
+              value="average 1.55"
+              name="activity"
+            />
+          </div>
 
-        1.2 - if you do not have physical activity and sedentary work
-      </label>
-      <input type="radio" id="low 1.2" value="low 1.2" name="activity" />
+          <div>
+            <label htmlFor="high 1.725">
+              1.725 - if you train fully 6-7 times a week
+            </label>
+            <input
+              type="radio"
+              id="high 1.725"
+              value="high 1.725"
+              name="activity"
+            />
+          </div>
 
-      <label htmlFor="light 1.375">
+          <div>
+            <label htmlFor="hard 1.9">
+              1.9 - if your work is related to physical labor, you train 2 times
+              a day and include strength exercises in your training program
+            </label>
+            <input
+              type="radio"
+              id="hard 1.9"
+              value="hard 1.9"
+              name="activity"
+            />
+          </div>
+        </div>
+      </UserInformationContainer>
 
-        1.375 - if you do short runs or light gymnastics 1-3 times a week
-      </label>
-      <input
-        type="radio"
-        id="light 1.375"
-        value="light 1.375"
-        name="activity"
-      />
-
-      <label htmlFor="average 1.55">
-     
-        1.55 - if you play sports with average loads 3-5 times a week
-      </label>
-      <input
-        type="radio"
-        id="average 1.55"
-        value="average 1.55"
-        name="activity"
-      />
-
-      <label htmlFor="high 1.725">
-        1.725 - if you train fully 6-7 times a week
-      </label>
-      <input type="radio" id="high 1.725" value="high 1.725" name="activity" />
-
-      <label htmlFor="hard 1.9">
-
-        1.9 - if your work is related to physical labor, you train 2 times a day
-        and include strength exercises in your training program
-      </label>
-      <input type="radio" id="hard 1.9" value="hard 1.9" name="activity" />
-
-      <button type="submit">Save</button>
-      <Link to={"/main"}>Cancel</Link>
-
-    </form>
+      <div>
+        <button type="submit">Save</button>
+        <Link to={'/main'}>Cancel</Link>
+      </div>
+    </UserInformationForm>
   );
 };
 export default UserInformation;
---
+++ src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.styled.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const UserInformationForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const UserInformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserInformationLabel = styled.label`
+  color: var(--color-primary-white);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.43;
+  margin-bottom: 12px;
+`;
+
+export const UserInformationInput = styled.input`
+  max-width: 300px;
+  height: 36px;
+  border-radius: 12px;
+  border: 1px solid var(--color-primary-grey);
+  background: var(--color-primary-black-2);
+  padding: 8px 10px;
+  color: var(--color-primary-white);
+`;
+
+export const UserInformationPhotoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const UserInformationIconDirect = styled.svg`
+  margin-right: 6px;
+  width: 16px;
+  height: 16px;
+`;
+export const UserInformationDownloadLabel = styled.label`
+  color: var(--color-primary-white);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.42;
+`;
