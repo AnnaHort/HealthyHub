cdefba1da870d34647940e326c84c63b7e79bcf5
Maksim Gulay <maksimgulaj@gmail.com>
2023-12-22 01:36:53 +0200

    fix path

--- src/pages/ProfileSettingsPage/ProfileSettingsPage.jsx
+++ src/pages/ProfileSettingsPage/ProfileSettingsPage.jsx
@@ -1,17 +1,22 @@
 // import ProfileSettingImg from './ProfileSettingImg';
 import {
+  Img,
   ProfileSettingsPageContainer,
-  ProfileSettingsPageImg,
+  // ProfileSettingsPageImg,
   ProfileSettingsPageTitle,
 } from './ProfileSettingsPage.styled';
 import UserInformation from './UserInformationComponent/UserInformation';
+import { ReactComponent as Images } from '../../img/YourActivityImg/Illustration.svg';
 
 const ProfileSettingsPage = () => {
   return (
     <ProfileSettingsPageContainer>
       <div>
         <ProfileSettingsPageTitle>Profile setting</ProfileSettingsPageTitle>
-        <ProfileSettingsPageImg src="/src/img/ProfileSettingPagesImg/Illustration.png" />
+        {/* <ProfileSettingsPageImg src="/src/img/ProfileSettingPagesImg/Illustration.png" /> */}
+        <Img>
+          <Images />
+        </Img>
       </div>
 
       <UserInformation />
