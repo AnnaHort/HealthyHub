94b0290b44733275c78bf5429f30bc371550faf8
AnnaHort <annahort3d@gmail.com>
2023-12-21 21:15:05 +0200

    finished mobile version profileSettingPages

--- src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.jsx
+++ src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.jsx
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom';
 import {
+  UserInformationBtnContainer,
   UserInformationContainer,
   UserInformationDownloadLabel,
   UserInformationForm,
   UserInformationIconDirect,
   UserInformationInput,
   UserInformationLabel,
+  UserInformationLabelRadio,
+  UserInformationLinkCancel,
   UserInformationPhotoContainer,
+  UserInformationRadioBtnContainer,
+  UserInformationRadioContainer,
+  UserInformationRadioInput,
+  UserInformationSaveBtn,
   UserInformationTitle,
 } from './UserInformation.styled';
 import { ImgContainer } from '../../../components/UserInfoNav/UserInfoNav.styled';
-import { GenderRadioBtnContainer } from '../../../components/SelectGender/SelectGender.styled';
 
 const UserInformation = () => {
   return (
@@ -21,6 +26,7 @@ const UserInformation = () => {
           type="text"
           id="name"
           placeholder="тут має бути імя користувача"
+          
         />
       </UserInformationContainer>
 
@@ -42,14 +48,22 @@ const UserInformation = () => {
               stroke="var(--color-primary-green-lite)"
             ></use>
           </UserInformationIconDirect>
-          <input type="file" id="photo" name="fileUpload" style={{position:"absolute", opacity:"0"}}/>
-          <UserInformationDownloadLabel htmlFor="photo">Download new photo</UserInformationDownloadLabel>
+          <input
+            type="file"
+            id="photo"
+            name="fileUpload"
+            style={{ position: 'absolute', opacity: '0' }}
+          />
+          <UserInformationDownloadLabel htmlFor="photo">
+            Download new photo
+          </UserInformationDownloadLabel>
         </UserInformationPhotoContainer>
       </UserInformationContainer>
 
       <UserInformationContainer>
         <UserInformationLabel htmlFor="age">Your age</UserInformationLabel>
         <UserInformationInput
+         placeholder="вік користувача"
           type="number"
           id="age"
           name="quantity"
@@ -60,101 +74,118 @@ const UserInformation = () => {
 
       <UserInformationContainer>
         <UserInformationTitle>Gender</UserInformationTitle>
-        <div>
-          <label htmlFor="male" style={{color:"red"}}>Male</label>
-          <input type="radio" id="male" name="gender" value="male" />
-          <label htmlFor="female" style={{color:"red"}}>Female</label>
-          <input type="radio" id="female" name="gender" value="female" />
-        </div>
+        <UserInformationRadioBtnContainer>
+          <UserInformationRadioContainer>
+            <UserInformationRadioInput type="radio" id="male" name="gender" value="male" />
+            <UserInformationLabelRadio htmlFor="male">Male</UserInformationLabelRadio>
+          </UserInformationRadioContainer>
+
+          <UserInformationRadioContainer>
+            <UserInformationRadioInput
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+            />
+            <UserInformationLabelRadio htmlFor="female">Female</UserInformationLabelRadio>
+          </UserInformationRadioContainer>
+        </UserInformationRadioBtnContainer>
       </UserInformationContainer>
 
       <UserInformationContainer>
         <UserInformationLabel htmlFor="height">Height</UserInformationLabel>
         <UserInformationInput
+        placeholder="ріст користувача"
           type="number"
           id="height"
           name="quantity"
           min="1"
-          max="100"
+          max="300"
         />
       </UserInformationContainer>
 
       <UserInformationContainer>
         <UserInformationLabel htmlFor="weight">Weight</UserInformationLabel>
         <UserInformationInput
+        placeholder="вага користувача"
           type="number"
           id="weight"
           name="quantity"
           min="1"
-          max="100"
+          max="300"
         />
       </UserInformationContainer>
 
       <UserInformationContainer>
-        <h3>Your Activity</h3>
+        <UserInformationTitle>Your Activity</UserInformationTitle>
         <div>
-          <div>
-            <label htmlFor="low 1.2">
+          <UserInformationRadioContainer style={{ position: 'relative' }}>
+            <UserInformationRadioInput
+              type="radio"
+              id="low 1.2"
+              value="low 1.2"
+              name="activity"
+            />
+            <UserInformationLabelRadio htmlFor="low 1.2">
               1.2 - if you do not have physical activity and sedentary work
-            </label>
-            <input type="radio" id="low 1.2" value="low 1.2" name="activity" />
-          </div>
+            </UserInformationLabelRadio>
+          </UserInformationRadioContainer>
 
-          <div>
-            <label htmlFor="light 1.375">
-              1.375 - if you do short runs or light gymnastics 1-3 times a week
-            </label>
-            <input
+          <UserInformationRadioContainer style={{ position: 'relative' }}>
+            <UserInformationRadioInput
               type="radio"
               id="light 1.375"
               value="light 1.375"
               name="activity"
             />
-          </div>
+            <UserInformationLabelRadio htmlFor="light 1.375">
+              1.375 - if you do short runs or light gymnastics 1-3 times a week
+            </UserInformationLabelRadio>
+          </UserInformationRadioContainer>
 
-          <div>
-            <label htmlFor="average 1.55">
-              1.55 - if you play sports with average loads 3-5 times a week
-            </label>
-            <input
+          <UserInformationRadioContainer style={{ position: 'relative' }}>
+            <UserInformationRadioInput
               type="radio"
               id="average 1.55"
               value="average 1.55"
               name="activity"
             />
-          </div>
+            <UserInformationLabelRadio htmlFor="average 1.55">
+              1.55 - if you play sports with average loads 3-5 times a week
+            </UserInformationLabelRadio>
+          </UserInformationRadioContainer>
 
-          <div>
-            <label htmlFor="high 1.725">
-              1.725 - if you train fully 6-7 times a week
-            </label>
-            <input
+          <UserInformationRadioContainer style={{ position: 'relative' }}>
+            <UserInformationRadioInput
               type="radio"
               id="high 1.725"
               value="high 1.725"
               name="activity"
             />
-          </div>
+            <UserInformationLabelRadio htmlFor="high 1.725">
+              1.725 - if you train fully 6-7 times a week
+            </UserInformationLabelRadio>
+          </UserInformationRadioContainer>
 
-          <div>
-            <label htmlFor="hard 1.9">
-              1.9 - if your work is related to physical labor, you train 2 times
-              a day and include strength exercises in your training program
-            </label>
-            <input
+          <UserInformationRadioContainer style={{ position: 'relative' }}>
+            <UserInformationRadioInput
               type="radio"
               id="hard 1.9"
               value="hard 1.9"
               name="activity"
             />
-          </div>
+            <UserInformationLabelRadio htmlFor="hard 1.9">
+              1.9 - if your work is related to physical labor, you train 2 times
+              a day and include strength exercises in your training program
+            </UserInformationLabelRadio>
+          </UserInformationRadioContainer>
         </div>
       </UserInformationContainer>
 
-      <div>
-        <button type="submit">Save</button>
-        <Link to={'/main'}>Cancel</Link>
-      </div>
+      <UserInformationBtnContainer>
+        <UserInformationSaveBtn type="submit">Save</UserInformationSaveBtn>
+        <UserInformationLinkCancel to={'/main'}>Cancel</UserInformationLinkCancel>
+      </UserInformationBtnContainer>
     </UserInformationForm>
   );
 };
--- src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.styled.js
+++ src/pages/ProfileSettingsPage/UserInformationComponent/UserInformation.styled.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const UserInformationForm = styled.form`
@@ -17,6 +18,7 @@ export const UserInformationLabel = styled.label`
   font-weight: 500;
   line-height: 1.43;
   margin-bottom: 12px;
+  font-family: Poppins;
 `;
 
 export const UserInformationInput = styled.input`
@@ -26,6 +28,17 @@ export const UserInformationInput = styled.input`
   background: var(--color-primary-black-2);
   padding: 8px 10px;
   color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.43;
+  &::placeholder {
+    color: var(--color-primary-white);
+    font-family: Poppins;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.43%;
+  }
 `;
 
 export const UserInformationPhotoContainer = styled.div`
@@ -42,11 +55,92 @@ export const UserInformationDownloadLabel = styled.label`
   font-size: 14px;
   font-weight: 400;
   line-height: 1.42;
+  font-family: Poppins;
 `;
 
 export const UserInformationTitle = styled.h3`
-color: var(--color-primary-white);
-font-size: 14px;
-font-weight: 500;
-line-height: 1.43;
-`
+  color: var(--color-primary-white);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.43;
+  margin-bottom: 12px;
+  font-family: Poppins;
+`;
+
+export const UserInformationRadioBtnContainer = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+export const UserInformationRadioInput = styled.input`
+  opacity: 0;
+  position: absolute;
+`;
+export const UserInformationLabelRadio = styled.label`
+  padding-left: 14px;
+  font-family: Poppins;
+  cursor: pointer;
+  color: var(--color-primary-white);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.43;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    left: 0;
+    width: 12px;
+    height: 12px;
+    border: 1px solid #b6b6b6;
+    background: #0f0f0f;
+    border-radius: 50%;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translate(-50%, -50%);
+    width: 6px;
+    height: 6px;
+    background: #e3ffa8;
+    border-radius: 50%;
+    opacity: 0;
+    transition: 0.2s ease-in;
+  }
+  input[type='radio']:checked + &::after {
+    opacity: 1;
+  }
+`;
+export const UserInformationRadioContainer = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+export const UserInformationBtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+export const UserInformationSaveBtn = styled.button`
+  border-radius: 12px;
+  background: var(--color-primary-green-lite);
+  border: none;
+  padding: 8px 10px;
+  color: var(--color-primary-black-2);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.43;
+  font-family: Poppins;
+`;
+
+export const UserInformationLinkCancel = styled(Link)`
+  text-align: center;
+  color: var(--color-primary-grey);
+  text-align: center;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.42;
+`;
