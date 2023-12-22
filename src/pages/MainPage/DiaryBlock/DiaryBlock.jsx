162573cc907d5e03c507ffb1fc6e9276bb89473f
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-22 17:37:10 +0200

    add diaryBlock files

---
+++ src/pages/MainPage/DiaryBlock/DiaryBlock.jsx
@@ -0,0 +1,16 @@
+import {
+  DiaryBlockContainer,
+  DiaryBlockTitle,
+  SeeMoreButton,
+} from './DiaryBlock.styled';
+
+const DiaryBlock = () => {
+  return (
+    <DiaryBlockContainer>
+      <DiaryBlockTitle>Diary</DiaryBlockTitle>
+      <SeeMoreButton to="/main/diary">See more</SeeMoreButton>
+    </DiaryBlockContainer>
+  );
+};
+
+export default DiaryBlock;
