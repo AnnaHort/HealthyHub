aa170bf5be7d6bfc8a1dfca284815d1b37ac5784
vovaukrainets <127029812+gitvova@users.noreply.github.com>
2023-12-22 17:58:08 +0200

    RecommendedFoodPage
    it was necessary to make a request to the server to receive pictures and data about the product and move the button to another page.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import WelcomePage from './pages/WelcomePage/welcomePage';
 import SignInPages from './pages/SignInPages/SignInPages';
 import ForgotPasswordPage from './components/ForgotPasswordPage/ForgotPasswordPage';
+import RecommendedFoodPage from './pages/RecommendedFood/RecommendedFoodPage';
 
 const SharedLayout = lazy(() =>
   import('./components/SharedLayout/SharedLayout')
@@ -16,12 +17,12 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-
             <Route path="/signin" element={<SignInPages />} />
+            <Route path="/signup" element={<RecommendedFoodPage />} />
             <Route path="/signup" element={<SignUpPage />} />
+
             <Route path="/welcome" element={<WelcomePage />} />
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-
           </Route>
           <Route path="*" element={<ErrorPage />} />
         </Routes>
