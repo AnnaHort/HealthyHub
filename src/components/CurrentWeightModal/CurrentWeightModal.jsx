8391ee42f13470b0a34402c36f90576931fbd241
AnnaHort <annahort3d@gmail.com>
2023-12-23 17:15:16 +0200

    update logic for currentWeightModal

--- src/components/CurrentWeightModal/CurrentWeightModal.jsx
+++ src/components/CurrentWeightModal/CurrentWeightModal.jsx
@@ -13,35 +13,57 @@ import {
   Title,
 } from './CurrentWeightModal.styled';
 import { ReactComponent as CloseCircle } from '../../img/Header/close-circle.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import {fetchCurentUser} from "/src/redux/auth/authOperations.js"
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUpdateUserStatus } from '../../redux/updateUser/updateSelectors';
+import { setUpdateUserFalse, updateUser } from '../../redux/updateUser/updateOperations';
 
 const CurrentWeightModal = ({ onCloseButtonClick }) => {
-  const [weight, setWeight] = useState();
+  const [weight, setWeight] = useState('');
+  const [userData, setUserData] = useState();
+
+  const userUpdate = useSelector(selectUpdateUserStatus);
   const dispatch = useDispatch()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const newUserData = {
       weight,
     };
+
     try {
       const response = await axios.put('/api/user/update', newUserData);
-      console.log(response.data);
       toast.success(response.data.message);
-      dispatch(fetchCurentUser())
+      dispatch(updateUser(newUserData));
+      dispatch(setUpdateUserFalse());
     } catch (error) {
       console.error('Data error', error.message);
-      toast.error('Error updating user information');
+        toast.error('Error updating user information');
     }
-  };
+  }
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('api/user/current');
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Data error', error.message);
+      }
+    };
+    fetchData();
+  }, [userUpdate]);
 
   const currentDate = new Date().toLocaleDateString();
   
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
   return (
+    
     <Container>
       <Div>
         <Title>Enter your current weight</Title>
@@ -66,7 +88,7 @@ const CurrentWeightModal = ({ onCloseButtonClick }) => {
           value={weight}
           onChange={(e) => setWeight(e.target.value)}
         />
-        <BtnConfirm type='submit' >
+        <BtnConfirm type='submit'>
           Confirm
         </BtnConfirm>
         <BtnCancel onClick={onCloseButtonClick}>Cancel</BtnCancel>
