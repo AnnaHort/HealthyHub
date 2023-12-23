737b333babce1f2f9231bd8f7dd3358e51b15088
vovaukrainets <127029812+gitvova@users.noreply.github.com>
2023-12-23 12:26:24 +0200

    RecommendedFood
    виправив відображення зображень на різних пристроях.

--- src/components/RecommendedFood.jsx/RecommendedFood.styled.js
+++ src/components/RecommendedFood.jsx/RecommendedFood.styled.js
@@ -1,48 +1,89 @@
 import styled from 'styled-components';
 
 export const RecommendedFoodContainer = styled.div`
-  width: 676px;
-  height: 230px;
-  margin-left: auto;
-  margin-right: auto;
+  width: 300px;
+  height: 218px;
   display: flex;
   flex-wrap: wrap;
-  gap: 20px;
+  @media (min-width: 834px) {
+    width: 780px;
+    height: 230px;
+    gap: 20px;
+  }
+  @media (min-width: 1440px) {
+    width: 676px;
+    height: 230px;
+    margin-left: 696px;
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
 
 export const RecommendedFoodFrame = styled.li`
-  width: 328px;
+  width: 300px;
   height: 86px;
-  padding: 20px 108px 20px 24px;
+  padding: 20px 126px 20px 24px;
   border-radius: 12px;
   background: var(--color-primary-black-2);
-  display: block;
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
   align-items: flex-start;
   justify-content: start;
+  margin-bottom: 10px;
+  @media (min-width: 834px) {
+    width: 380px;
+    margin-bottom: 0px;
+  }
+  @media (min-width: 1440px) {
+    width: 328px;
+    padding: 20px 108px 20px 24px;
+    border-radius: 12px;
+    background: var(--color-primary-black-2);
+    display: block;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: start;
+  }
 `;
 
 export const RecommendedFoodUnitName = styled.h2`
   color: var(--color-primary-white);
   font-family: Poppins;
-  font-size: 22px;
+  font-size: 18px;
   font-style: normal;
   font-weight: 400;
-  line-height: 1.45;
-  text-align: center;
+  line-height: 1.33;
+  margin-top: 20px;
+  margin-bottom: 16px;
+  @media (min-width: 834px) {
+    color: var(--color-primary-white);
+    font-size: 22px;
+    font-weight: 400;
+    line-height: 1.45;
+    margin-left: 696px;
+  }
 `;
 
 export const RecommendedFoodName = styled.h3`
   color: var(--color-primary-white);
-  /* Headline text/H3 */
   font-family: Poppins;
   font-size: 18px;
   font-style: normal;
   font-weight: 500;
   line-height: 1.33;
   margin-left: 20px;
+  @media (min-width: 1440px) {
+    color: var(--color-primary-white);
+    font-family: Poppins;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.33;
+    margin-left: 20px;
+  }
 `;
 
 export const RecommendedFoodImg = styled.img`
@@ -55,6 +96,7 @@ export const TextContainer = styled.div`
 `;
 
 export const RecommendedFoodWeight = styled.p`
+  width: 36px;
   color: var(--color-primary-white);
   font-family: Poppins;
   font-size: 14px;
@@ -65,6 +107,7 @@ export const RecommendedFoodWeight = styled.p`
 `;
 
 export const RecommendedFoodCalories = styled.p`
+  width: 88px;
   color: var(--color-primary-grey);
   font-family: Poppins;
   font-size: 14px;
